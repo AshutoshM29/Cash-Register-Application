b81c8bf707021715183066838ffa010d05a2b1e2
Ashutosh Mishra <ashutoshrm01@gmail.com>
2021-09-11 18:34:33 +0530

    feat  : next button

--- app.js
+++ app.js
@@ -1,18 +1,38 @@
 const billAmount = document.querySelector("#bill-amount");
 const cashGiven = document.querySelector("#cash-given");
+
+const cashGivenPart = document.querySelector("#cash-given-part");
+
 const checkButton = document.querySelector("#check-button");
+const nextButton = document.querySelector("#next-button");
+
+
 const message = document.querySelector("#error-message");
+const outputDiv = document.querySelector("#output");
 const noOfNotes = document.querySelectorAll(".no-of-notes");
 const availableNotes = [2000, 500, 100, 20, 10, 5, 1];
+
+
+nextButton.addEventListener("click", function showNext() {
+    hideMessage();
+    if(billAmount.value > 0) {
+        nextButton.style.display="none";
+        cashGivenPart.style.display = "block";
+        outputDiv.style.display ="block";
+
+    } else {
+        showMessage("Invalid Bill Amount");
+    }
+})
 
 checkButton.addEventListener("click", function validateBillAndCashAmount() {
     hideMessage();
     if (billAmount.value > 0) {
         if (cashGiven.value >= billAmount.value) {
             var amountToBeReturned = cashGiven.value - billAmount.value;
-            calculateChange(amountToBeReturned);
+            calculateChange(amountToBeReturned > 0);
         } 
-            else {
+        else {
             showMessage("You have given less cash!");
         }
 
